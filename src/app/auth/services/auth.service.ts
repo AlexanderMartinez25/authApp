5a1f2489f27056471910c6183719f91095743548
Alexander <alexander.martinez@lapatota.cl>
2022-07-26 23:39:57 -0400

    Validar token - AuthGuard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, tap } from 'rxjs/operators'
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { AuthResponse, Usuario } from '../interfaces/interfaces';
@@ -45,13 +45,24 @@ export class AuthService {
 
   }
 
-  validarToken() {
+  validarToken(): Observable<boolean> {
 
     const url = `${this.baseUrl}/auth/renew`;
     const headers = new HttpHeaders()
       .set('x-token', localStorage.getItem('token') || '');
 
-    return this.http.get(url, { headers });
+    return this.http.get<AuthResponse>(url, { headers })
+      .pipe(
+        map(resp => {
+          localStorage.setItem('token', resp.token!);
+          this._usuario = {
+            name: resp.name!,
+            uid: resp.uid!
+          }
+          return resp.ok!;
+        }),
+        catchError(err => of(false))
+      )
   }
 }
 
